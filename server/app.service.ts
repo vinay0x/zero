ca1d2f51e69a76d5e229cd1da0277c6c11f10421
Vinay Chandran <vinaychandranvs@gmail.com>
2021-07-15 11:53:38 +0530

    Move CLI to dependency

--- server/app.service.ts
+++ server/app.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import assetJson from "../assets.json"
 
+type InitialRenderData = {
+  bundlePath: string,
+}
+
 @Injectable()
 export class AppService {
   constructor(private configService: ConfigService) { }
 
-  renderApp(): object {
+  renderApp(): InitialRenderData {
     const env = this.configService.get("NODE_ENV")
     const assetPrefix = env == "development" ? "http://localhost:4000/" : "/dist/";
     return { bundlePath: assetPrefix + assetJson.main.js }
