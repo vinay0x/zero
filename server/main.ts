6779c30e05136074ad255a426713cc63a276e522
Vinay Chandran <vinaychandranvs@gmail.com>
2021-07-15 12:28:36 +0530

    Fix failing import

--- server/main.ts
+++ server/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { resolve } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
