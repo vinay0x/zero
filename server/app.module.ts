56f3168668eb5e8cfe6439abec5424a7aef0dc9e
Vinay Chandran <vinaychandranvs@gmail.com>
2021-07-16 11:13:28 +0530

    Add auth modules

--- server/app.module.ts
+++ server/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AccountModule } from 'server/modules/account/account.module';
 import { PrismaModule } from 'server/database/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, AccountModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, AccountModule, AuthModule, UsersModule],
 })
 export class AppModule { }
