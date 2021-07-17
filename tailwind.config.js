c812746b3b43839798d3227b085d242ee7d7bddd
Vinay Chandran <vinaychandranvs@gmail.com>
2021-07-17 19:43:30 +0530

    Fix purge path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.tsx'],
+  purge: ['./client/**/*.html', './client/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
