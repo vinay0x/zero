4862b20b8145761e95580284f8d3b34a26cefc20
Vinay Chandran <vinaychandranvs@gmail.com>
2021-07-18 13:32:12 +0530

    Update user.ts

--- server/types/user.ts
+++ server/types/user.ts
@@ -1,6 +1,6 @@
 import { Prisma } from '@prisma/client'
 
-// 1: Define a type that includes the relation to `Post`
+// 1: Define a type that includes the relation to `Organization`
 const userWithOrganizations = Prisma.validator<Prisma.UserArgs>()({
   include: {
     memberships: { include: { organization: true } }
